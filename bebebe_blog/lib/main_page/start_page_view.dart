bc5a4be46ac5fb947fb8851ca718998421f3b756
beyanon <tokabe333@gmail.com>
2024-03-04 22:31:52 +0900

    feat : デモの比率ガバガバ(位置はstackで無理やりできる)

--- bebebe_blog/lib/main_page/start_page_view.dart
+++ bebebe_blog/lib/main_page/start_page_view.dart
@@ -5,6 +5,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:rive/rive.dart';
+import 'dart:math' as math;
 
 import '../main_frame.dart';
 
@@ -27,6 +28,12 @@ class StartPageView extends State<StartPageWidget> with TickerProviderStateMixin
   /// でも再生する？
   bool isPlayDemo = true;
 
+  /// デモのサイズ(元のriveファイル)
+  double demoOriginHeight = 340;
+
+  /// でものサイズ(元のriveファイル)
+  double demoOriginWidth = 500;
+
   StartPageView(bool this.isPlayDemo) {}
 
   @override
@@ -38,14 +45,34 @@ class StartPageView extends State<StartPageWidget> with TickerProviderStateMixin
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    double displayWidth = MediaQuery.of(context).size.width;
+    double displayHeight = MediaQuery.of(context).size.height;
+
+    // でも画面のサイズを決める
+    double demoWidth = 0, demoHeight = 0;
+    double leftpadding = 0;
+    if (displayWidth <= displayHeight) {
+      demoWidth = displayWidth;
+      demoHeight = demoWidth / 500 * 340;
+    } else {
+      demoHeight = displayHeight;
+      demoWidth = math.min(displayWidth, demoHeight / 340 * 500);
+      leftpadding = (displayWidth - demoWidth) / 2;
+    }
+
+    print("display:${displayWidth},${displayHeight}  demo:${demoWidth},${demoHeight}");
+
+    Widget demo = this.isPlayDemo ? playDemo() : noPlayDemo();
 
     return Container(
-      height: height - 60,
-      width: width,
+      height: displayHeight - 60,
+      width: displayWidth,
       color: const Color.fromARGB(255, 137, 194, 240),
-      child: this.isPlayDemo ? playDemo() : noPlayDemo(),
+      child: Stack(
+        children: [
+          Positioned(left: leftpadding, top: 0, child: Container(width: demoWidth, height: demoHeight, child: demo))
+        ],
+      ),
     );
   } // end of method
 
