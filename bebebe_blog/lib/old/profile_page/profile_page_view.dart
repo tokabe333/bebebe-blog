3b7f35660c573cb29ec3d275d8bebb118b26c0de
tokabe333 <tokabe333@gmail.com>
2024-03-06 22:52:06 +0900

    feat : 全ページをスクロールするサイトにしたい

---
+++ bebebe_blog/lib/old/profile_page/profile_page_view.dart
@@ -0,0 +1,22 @@
+/// -------------------------------------------
+///           自己紹介About Me的な
+/// -------------------------------------------
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../main_frame.dart';
+
+class ProfilePageWidget extends StatefulWidget {
+  ProfilePageWidget({Key? key}) : super(key: key);
+
+  State<ProfilePageWidget> createState() => ProfilePageView();
+} // end of class
+
+class ProfilePageView extends State<ProfilePageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+} // end of class
