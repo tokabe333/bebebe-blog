0021b67a3ebb5466c6173467a190301a7c146828
tokabe333 <tokabe333@gmail.com>
2024-03-07 23:01:24 +0900

    add : 経歴とかのウィジェットをうつす

---
+++ bebebe_blog/lib/contents/profile_page/profile_introduce.dart
@@ -0,0 +1,15 @@
+/// -------------------------------------------
+///         自己紹介文章のところ
+/// -------------------------------------------
+
+import 'package:bebebe_blog/__old/profile_page/profile_page_view.dart';
+import 'package:bebebe_blog/contents/profile_page/skilset_badge.dart';
+import 'package:easy_animate/animation/fade_in_animation.dart';
+import 'package:easy_animate/enum/animate_direction.dart';
+import 'package:flutter/material.dart';
+
+import './profile_page_view.dart';
+
+extension ProfileIntroduce on ProfilePageView {} // end of class
+
+
