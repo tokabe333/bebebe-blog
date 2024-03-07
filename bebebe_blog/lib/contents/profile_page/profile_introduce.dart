7cf5073b62688d7b87e3a62466fa006901baa7ad
tokabe333 <tokabe333@gmail.com>
2024-03-07 23:38:50 +0900

    feat : 紹介文テキストの改行とフォントサイズ調整OK

--- bebebe_blog/lib/contents/profile_page/profile_introduce.dart
+++ bebebe_blog/lib/contents/profile_page/profile_introduce.dart
@@ -2,7 +2,6 @@
 ///         自己紹介文章のところ
 /// -------------------------------------------
 
-import 'package:bebebe_blog/__old/profile_page/profile_page_view.dart';
 import 'package:bebebe_blog/contents/profile_page/skilset_badge.dart';
 import 'package:easy_animate/animation/fade_in_animation.dart';
 import 'package:easy_animate/enum/animate_direction.dart';
@@ -10,5 +9,35 @@ import 'package:flutter/material.dart';
 
 import './profile_page_view.dart';
 
-extension ProfileIntroduce on ProfilePageView {} // end of class
+extension ProfileIntroduce on ProfilePageView {
+  /// 紹介文作成
+  Widget createIntroduce(BuildContext context) {
+    /// 画面サイズからコンテンツサイズを作る
+    double displayWidth = MediaQuery.of(context).size.width;
+    double contentWidth = displayWidth * 0.95;
+    double paddingWidth = (displayWidth - contentWidth) / 2;
+
+    return ConstrainedBox(
+      constraints: BoxConstraints.tightFor(width: contentWidth),
+      child: Container(
+          margin: EdgeInsets.only(left: paddingWidth, right: paddingWidth),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 233, 233, 233),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            "とりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすととりあえずテキストとりあえずてきすと",
+            style: TextStyle(
+              fontFamily: "noto",
+              // color: Colors.white,
+              color: Colors.black,
+              letterSpacing: 0.5,
+              fontWeight: FontWeight.w500,
+              fontSize: displayWidth > 500 ? 15 : 13,
+            ),
+          )),
+    );
+  } // end of method
+} // end of class
 
--- bebebe_blog/lib/contents/profile_page/profile_page_view.dart
+++ bebebe_blog/lib/contents/profile_page/profile_page_view.dart
@@ -2,12 +2,13 @@
 ///         自己紹介とスキルセットとか
 /// -------------------------------------------
 
-import 'package:bebebe_blog/contents/profile_page/skilset_badge.dart';
 import 'package:easy_animate/animation/fade_in_animation.dart';
 import 'package:easy_animate/enum/animate_direction.dart';
 import 'package:flutter/material.dart';
 
 import './profile_page_view.dart';
+import './skilset_badge.dart';
+import './profile_introduce.dart';
 
 class ProfilePageWidget extends StatefulWidget {
   ProfilePageWidget({Key? key}) : super(key: key);
@@ -22,14 +23,20 @@ class ProfilePageView extends State<ProfilePageWidget> {
   /// メインコンテンツ(デモとサイト紹介)を作成
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SkilsetBadge(
-        text: "Github",
-        fontSize: 20,
-        hyperLink: "https://github.com/tokabe333",
-        borderColor: Color.fromARGB(255, 0, 112, 19),
-        hoverBackgroundColor: Color.fromARGB(255, 159, 203, 166),
-      ),
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        this.createIntroduce(context),
+        Center(
+          child: SkilsetBadge(
+            text: "Github",
+            fontSize: 20,
+            hyperLink: "https://github.com/tokabe333",
+            borderColor: Color.fromARGB(255, 0, 112, 19),
+            hoverBackgroundColor: Color.fromARGB(255, 159, 203, 166),
+          ),
+        ),
+      ],
     );
   }
 } // end of class
