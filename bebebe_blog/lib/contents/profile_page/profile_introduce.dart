bc86feb205a8fb44c9eecb5fd992f2b9a267c60b
tokabe333 <tokabe333@gmail.com>
2024-03-10 21:32:48 +0900

    refactor : 紹介文変更

--- bebebe_blog/lib/contents/profile_page/profile_introduce.dart
+++ bebebe_blog/lib/contents/profile_page/profile_introduce.dart
@@ -27,7 +27,7 @@ extension ProfileIntroduce on ProfilePageView {
       child: Padding(
         padding: EdgeInsets.only(top: 15),
         child: Text(
-          "高専→高専専攻科→大学院と情報系学科に所属。就職を機に地元へ帰るも半年で離職し、四国八十八箇所へ歩き遍路に。結願後は友人に誘われ再就職して現在。競技プログラミングやUnityでのゲーム開発、Djangoでのバックエンド、Flutterでのフロントエンド等スキルは雑多。大学院では医療用CTの画像再構成と深層学習の研究に従事。",
+          "高専→高専専攻科→大学院と情報系学科に所属。就職を機に地元へ帰るも半年で離職し、四国八十八箇所へ歩き遍路に。結願後は友人に誘われ再就職して現在。スキルは競技プログラミングやUnityでのゲーム開発、Djangoでのバックエンド、Flutterでのフロントエンド等。大学院では医療用CTの画像再構成と深層学習の研究に従事。",
           style: GoogleFonts.kosugiMaru(
             // style: GoogleFonts.notoSansJp(
             color: Colors.black,
