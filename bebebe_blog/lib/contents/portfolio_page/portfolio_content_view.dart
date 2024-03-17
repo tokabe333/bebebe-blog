97961c0ac6478585c916f87d4e500533ee4e0587
beyanon <tokabe333@gmail.com>
2024-03-17 15:43:42 +0900

    feat : ホバー時のサイズ変更OK

--- bebebe_blog/lib/contents/portfolio_page/portfolio_content_view.dart
+++ bebebe_blog/lib/contents/portfolio_page/portfolio_content_view.dart
@@ -26,25 +26,66 @@ class PortFolioContentWidget extends StatefulWidget {
 } // end of class
 
 class PortFolioContentView extends State<PortFolioContentWidget> {
+  /// このコンテナの高さ(ホバー時に変化する)
+  double _containerHeight = 0;
+
+  /// このコンテナの横幅(ホバー時に変化する)
+  double _containerWidth = 0;
+
+  /// コンテナのホバー中検出
+  bool _isHover = false;
+
+  @override
+  initState() {
+    this._containerHeight = widget.containerHeight;
+    this._containerWidth = widget.containerWidth;
+
+    super.initState();
+  }
+
   /// フェードインアニメーションを付けてコンテナを返す
   @override
   Widget build(BuildContext context) {
+    // ホバー時とクリック時のイベントをつける
+    Widget container = InkWell(
+      onTap: () => {},
+      onHover: (isEnter) {
+        if (isEnter) {
+          this._isHover = true;
+        } else {
+          this._isHover = false;
+        }
+        setState(() {});
+      },
+      child: this._createMainContainer(),
+    );
+
     return FadeInAnimation(
       animateDirection: AnimateDirection.bottom,
       moveAmount: 10,
       delay: const Duration(milliseconds: 1000),
-      child: this._createMainContainer(),
+      child: container,
     );
   } // end of build
 
   /// 実際に描画するコンテナを作る
   Widget _createMainContainer() {
-    return Container(
-      height: widget.containerHeight,
-      width: widget.containerWidth,
+    return AnimatedContainer(
+      // ホバー時のアニメーション設定
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeOutCubic,
+      // ホバー時のサイズ変更
+      height: this._isHover ? this._containerHeight * 1.1 : this._containerHeight,
+      width: this._isHover ? this._containerWidth * 1.1 : this._containerWidth,
       padding: const EdgeInsets.only(top: 5, bottom: 0, left: 10, right: 10),
       alignment: Alignment.topCenter,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: const Color(0xFFFFFFFF)),
+      // 形状と影
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color(0xFFFFFFFF),
+        boxShadow: this._isHover ? [const BoxShadow(color: Color.fromARGB(255, 210, 226, 250), blurRadius: 1, offset: Offset(3, 3))] : [],
+      ),
+      // サムネ画像とキャプションを並べる
       child: Column(children: [
         Image.asset(widget.imagePath),
         const Spacer(),
